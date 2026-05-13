12d45140809bf6964e197d8a81354f5f1917dbc9
Rian Ganesh <rianganesh64@gmail.com>
2026-05-13 13:14:33 -0400

    Finishing formulas for call options for black_scholes.py
    Finished (and reformatted for clarity) call formulas for d1, d2, and black-scholes. Tested with example variables. Also added rounding to two decimals for easier reading.

--- src/black_scholes.py
+++ src/black_scholes.py
@@ -1,14 +1,37 @@
 import numpy as np
 from scipy.stats import norm
 
-#Defining Variables
+#Defining Variables: Tests for right now
 price_underlying = 100
 strike_price = 100
 time = 1
 risk_free_rate = 0.05
 annualized_volatility = 0.2
-#Defining Helper Functions
 
-def d_one(): #intermediate value
-    return((np.log(price_underlying / strike_price) + time*(risk_free_rate + ((annualized_volatility**2)/2))) / (annualized_volatility * np.sqrt(time)))
-print(d_one())
+#CALL OPTIONS: The right to purchase an asset a a specific strike price
+#Defining Helper Functions
+def d_one_call():
+    return((np.log(price_underlying / strike_price)
+            + time*(risk_free_rate + ((annualized_volatility**2)/2)))
+            / (annualized_volatility * np.sqrt(time)))
+rounded_d_one_call = round(d_one_call(), 2)
+print(d_one_call())
+print(rounded_d_one_call)
+
+def d_two_call():
+    return((np.log(price_underlying / strike_price)
+            + time*(risk_free_rate - ((annualized_volatility**2)/2)))
+            / (annualized_volatility * np.sqrt(time)))
+rounded_d_two_call = round(d_two_call(), 2)
+print(d_two_call())
+print(rounded_d_two_call)
+
+#Putting it all together with Black-Scholes 
+def black_scholes_call():
+    return((norm.cdf(d_one_call()) * price_underlying)
+           - norm.cdf(d_two_call())
+           * strike_price * (np.e**((-1 * risk_free_rate)
+                                    * time)))
+rounded_black_scholes_call = round(black_scholes_call(), 2)
+print(black_scholes_call())
+print(rounded_black_scholes_call)
