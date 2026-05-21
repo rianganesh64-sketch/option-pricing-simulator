949e20930581b92eea828f2d04f0833fb375e2d2
Rian Ganesh <rianganesh64@gmail.com>
2026-05-20 22:47:35 -0400

    Update monte_carlo.py
    Used gbm formula for stock prices to create n amount of stock final prices. Created call_payoffs and put_payoffs to calculate the payoff values for those stock prices for a call and put option. Temporarily displaying outputs just to verify if everything is working.

--- src/monte_carlo.py
+++ src/monte_carlo.py
@@ -40,8 +40,45 @@ def validate_inputs(S, K, T, r, sigma, N):
     if N <= 0:
         raise ValueError ('N (number of simulations) must be positive')
 
+def simulate_final_prices(S, K, T, r, sigma, N):
+    validate_inputs(S, K, T, r, sigma, N)
 
-    
+    rng = np.random.default_rng(seed=42)
+    z_values = rng.standard_normal(N)
 
+    final_prices = S * np.exp(
+        (r - (0.5 * (sigma ** 2)))*T
+        + (sigma * (np.sqrt(T)) * z_values))
+    return(final_prices)
 
+def call_payoffs(S, K, T, r, sigma, N):
+    validate_inputs(S, K, T, r, sigma, N)
+    simulated_prices = simulate_final_prices(S, K, T, r, sigma, N)
+    call_payoff_values = np.maximum(simulated_prices - K, 0)
+    return(call_payoff_values)
 
+def put_payoffs(S, K, T, r, sigma, N):
+    validate_inputs(S, K, T, r, sigma, N)
+    simulated_prices = simulate_final_prices(S, K, T, r, sigma, N)
+    put_payoff_values = np.maximum(K - simulated_prices, 0)
+    return(put_payoff_values)
+
+if __name__ == "__main__":
+    final_prices = simulate_final_prices(S, K, T, r, sigma, N)
+    print("Final prices from GBM")
+    print(final_prices[:5])
+    print(np.mean(final_prices))
+    print(np.max(final_prices))
+    print(np.min(final_prices))
+    print("Payoffs for call options")
+    call_option_payoffs = call_payoffs(S, K, T, r, sigma, N)
+    print(call_option_payoffs[:5])
+    print(np.mean(call_option_payoffs))
+    print(np.max(call_option_payoffs))
+    print(np.min(call_option_payoffs))
+    print("Payoffs for put options")
+    put_option_payoffs = put_payoffs(S, K, T, r, sigma, N)
+    print(put_option_payoffs[:5])
+    print(np.mean(put_option_payoffs))
+    print(np.max(put_option_payoffs))
+    print(np.min(put_option_payoffs))
