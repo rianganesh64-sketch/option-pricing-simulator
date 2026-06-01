88132d1a8b220dcc6996b63054a95df235f701e7
Rian Ganesh <rianganesh64@gmail.com>
2026-05-31 22:39:25 -0400

    Planning for utils.py
    small commit, just writing down ideas

---
+++ src/utils.py
@@ -0,0 +1,7 @@
+#What to include in utils.py
+#1. absolute error (black_scholes - monte carlo)
+#2. percent error (absolute error / black_scholes) * 100
+#3. runtime measurement
+#4. currency formatting (for app polish)
+#5. format percent value
+#can use lambda for 4 and 5
