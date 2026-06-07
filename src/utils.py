e89f006802a9e1eed6afb983f5b5bae608d3b964
Rian Ganesh <rianganesh64@gmail.com>
2026-06-06 21:16:04 -0400

    finishing up utils.py
    Finish functions and temporary test cases found in utils.py. this file now includes a runtime function, percent error, absolute error, currency formatting, and percent formatting

--- src/utils.py
+++ src/utils.py
@@ -2,17 +2,9 @@ import time
 from src.black_scholes import black_scholes_call, black_scholes_put, black_scholes_price
 from src.monte_carlo import monte_carlo_price
 from src.gbm import generate_gbm_paths
-# python -m src.utils
 
-#What to include in utils.py
-#1. absolute error (black_scholes - monte carlo)
-#2. percent error (absolute error / black_scholes) * 100
-#3. runtime measurement
-#4. currency formatting (for app polish)
+
 #5. format percent value
-#can use lambda for 4 and 5
-
-#work on next: absolute error = |Black-Scholes price - Monte Carlo price|
 
 def measure_runtime(function_to_time, *args, **kwargs):
     start_time = time.perf_counter()
@@ -28,15 +20,25 @@ def absolute_error(S, K, T, r, sigma, N, option_type):
     abs_error = abs(bs_price - mc_price)
     return abs_error
 
+def percent_error(S, K, T, r, sigma, N, option_type):
+    abs_error = absolute_error(S, K, T, r, sigma, N, option_type)
+    bs_price = black_scholes_price(S, K, T, r, sigma, option_type)
+    if bs_price == 0:
+        return float("inf")
+    else:
+        per_error = (abs_error / bs_price) * 100
+    return per_error
+
+def format_currency(c):
+    formatted = "$" + (f"{c:,.2f}")
+    return formatted
+def format_percent(p):
+    formatted = (f"{p:.2f}") + "%"
+    return formatted
+
 #temporary test cases
 if __name__ == "__main__":
     #Testing a simple, addition function
-    print("testing add_multiple function")
-    def add_multiple(*args):
-        return sum(args)
-    result, execution_time = measure_runtime(add_multiple, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    print(result)
-
     print("testing black_scholes_call runtime")
     result, execution_time = measure_runtime(
         black_scholes_call,
@@ -53,7 +55,7 @@ if __name__ == "__main__":
 
     print("testing monte_carlo_price for put")
     result, execution_time = measure_runtime(
-        monte_carlo_price, 50, 50, 9/12, 0.04, 0.30, 100, "put"
+        monte_carlo_price, 50, 50, 9/12, 0.04, 0.30, 1000, "put"
     )
     print(result)
 
@@ -71,3 +73,12 @@ if __name__ == "__main__":
     print("\ntesting absolute error function:")
     print(absolute_error(110, 100, 1, 0.04, 0.05, 10, "call"))
     print(absolute_error(100, 10, 1, 0.05, 0.05, 10000, "call"))
+    print("\n testing percent error function:")
+    print(percent_error(110, 100, 1, 0.04, 0.05, 10, "call"))
+    print(percent_error(110, 100, 1, 0.04, 0.05, 10000, "call"))
+    print("\ntesting format_currency function")
+    print(format_currency(10.4732932883209342))
+    print(format_currency(black_scholes_price(100, 110, 1, 0.04, 0.04, "call")))
+    print("\ntest format_percent")
+    print(format_percent(19.2321212))
+    print(format_percent(percent_error(110, 100, 1, 0.04, 0.05, 100, "call")))
