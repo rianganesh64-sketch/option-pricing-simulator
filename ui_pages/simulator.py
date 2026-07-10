a7a49aa31fbf771a0431a935389c4aa65c895aae
Rian Ganesh <rianganesh64@gmail.com>
2026-07-10 10:37:54 -0400

    Update simulator.py with input widgets
    Inputs for stock price, strike price, time till expiration, stock volatility, risk-free rate, and number of simulations, option type

--- ui_pages/simulator.py
+++ ui_pages/simulator.py
@@ -6,7 +6,7 @@ def display_simulator():
     <style>
         .vertical-divider {
             border-left: 2px solid #fafafa;
-            height: 450px;
+            height: 1000px;
             margin: 0 auto;
         }
     </style>
@@ -33,15 +33,45 @@ def display_simulator():
     st.space("medium")
     st.title("Welcome to the Simulator!", text_alignment="center")
     st.space("medium")
-    input_col, divider_col, output_col = st.columns([1, 0.05, 2.5])
+    input_col, divider_col, output_col = st.columns([1.2, 0.05, 2.5])
     with input_col:
         st.header("Inputs", text_alignment="center")
         st.space("medium")
-        st.write("Stock price input will go here")
-        st.write("strike price input goes here")
-        st.write("volatility slider goes here")
-        st.space("large")
-        st.write("run buttons go here")
+        stock_input = st.number_input(
+            "Enter Current Stock Price:", min_value = 0.01, value=100.00)
+        st.write("Entered Stock Price: ", stock_input)
+        st.space("xxsmall")
+        strike_input = st.number_input(
+            "Enter Option Strike Price:", min_value = 0.01, value=110.00
+        )
+        st.write("Entered Strike Price:", strike_input)
+        st.space("xxsmall")
+        exp_time = st.slider("How long until the Option expires? (months)", 1, 60, value=12)
+        st.write("Entered Time Till Expiration:", exp_time)
+        st.space("xxsmall")
+        volatility = st.slider("Stock Volatility (%):", 0, 100, value=15)
+        st.write("Entered Stock Volatility:", volatility,"%")
+        st.space("xxsmall")
+        risk_free_rate_input = st.slider("Risk-free Rate (%)", 0.00, 15.00, step=0.25)
+        st.write("Entered Risk-free rate:", risk_free_rate_input,"%")
+        st.space("xxsmall")
+        num_simulations = st.slider("Number of Monte-carlo simulations", 10, 10000, step = 50)
+        st.write("Entered Number of Simulations:", num_simulations)
+        st.caption("Lower simulation counts usually create noisier, less stable estimates. More simulations can improve accuracy, but they also take longer to run. Try changing this value and see what happens!", text_alignment="center")
+        st.space("xxsmall")
+        option_type = st.radio(
+            "Call or Put Option?:",
+            ["**Call**", "**Put**"],
+            horizontal=True
+        )
+        st.space("xxsmall")
+        run_bs = st.button("Run Black-Scholes", use_container_width=True)
+        run_mc = st.button("Run Monte Carlo Simulation", use_container_width=True)
+
+        if run_bs:
+            st.write("Black-scholes button clicked")
+        if run_mc:
+            st.write("Monte-carlo button clicked")
     with divider_col:
       st.html(
           """
@@ -54,3 +84,4 @@ def display_simulator():
         st.space("medium")
         st.write("black-scholes price goes here: (formatting functions used)")
         st.write("monte_carlo price goes here: (formatting functions used)")
+
